c19320c5f30890f00264fd84ac42a6611823ab80
Anton Pedan <77937504+snap515@users.noreply.github.com>
2023-01-04 14:12:47 +0100

    2/10 task complete

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,18 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+const ingredientsEl = document.querySelector("#ingredients");
+
+ingredients.forEach((ingredient) => {
+  const ingredientEl = document.createElement("li");
+  ingredientEl.textContent = ingredient;
+  ingredientEl.classList.add("list");
+  ingredientsEl.appendChild(ingredientEl);
+});
+
+console.log(ingredientsEl);
