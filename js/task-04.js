fa04178498f41e2ed49e54e6f59fcbed53169ea8
Anton Pedan <77937504+snap515@users.noreply.github.com>
2023-01-06 12:41:41 +0100

    update 4/10 (callback function instead of anonymous function))

--- js/task-04.js
+++ js/task-04.js
@@ -1,15 +1,34 @@
-const decrementButtonClickHandler = document.querySelector(
-  '[data-action="decrement"]'
-);
-const incrementButtonClickHandler = document.querySelector(
-  '[data-action="increment"]'
-);
+//  ---------с анонимной функцией----------
+
+// const decrementButtonClickHandler = document.querySelector(
+//   '[data-action="decrement"]'
+// );
+// const incrementButtonClickHandler = document.querySelector(
+//   '[data-action="increment"]'
+// );
+// const valueEl = document.querySelector("#value");
+
+// incrementButtonClickHandler.addEventListener("click", () => {
+//   valueEl.textContent = Number(valueEl.textContent) + 1;
+// });
+
+// decrementButtonClickHandler.addEventListener("click", () => {
+//   valueEl.textContent = Number(valueEl.textContent) - 1;
+// });
+
+// -----------c колбек функцией-----------
+
+const decrementButtonEl = document.querySelector('[data-action="decrement"]');
+const incrementButtonel = document.querySelector('[data-action="increment"]');
 const valueEl = document.querySelector("#value");
 
-incrementButtonClickHandler.addEventListener("click", () => {
+function incrementButtonClickHandler(event) {
   valueEl.textContent = Number(valueEl.textContent) + 1;
-});
+}
 
-decrementButtonClickHandler.addEventListener("click", () => {
+function decrementButtonClickHandler(event) {
   valueEl.textContent = Number(valueEl.textContent) - 1;
-});
+}
+
+decrementButtonEl.addEventListener("click", decrementButtonClickHandler);
+incrementButtonel.addEventListener("click", incrementButtonClickHandler);
