45c9621f0982acc78b79f1a05dfdc97d3faed6eb
Anton Pedan <77937504+snap515@users.noreply.github.com>
2023-01-06 14:22:09 +0100

    5/10 task complete

---
+++ js/task-05.js
@@ -0,0 +1,11 @@
+const nameInputEl = document.querySelector("#name-input");
+const nameOutputEl = document.querySelector("#name-output");
+
+nameInputEl.addEventListener("input", currentTargetOutputChanger);
+
+function currentTargetOutputChanger(event) {
+  event.currentTarget.value === ""
+    ? (nameOutputEl.textContent = "Anonymous")
+    : (nameOutputEl.textContent = event.currentTarget.value);
+  console.dir(nameOutputEl.textContent);
+}
