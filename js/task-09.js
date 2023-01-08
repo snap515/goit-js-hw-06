2b7843b0cce2b71bf692ab762213f11b3439f14f
Anton Pedan <77937504+snap515@users.noreply.github.com>
2023-01-08 13:43:00 +0100

    9/10 task complete

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
+const bodyEl = document.querySelector("body");
+const changeColorBtnEl = document.querySelector(".change-color");
+const colorNameEl = document.querySelector(".color");
+
+changeColorBtnEl.addEventListener("click", bodyColorChanger);
+
+function bodyColorChanger(event) {
+  colorNameEl.textContent = getRandomHexColor();
+  bodyEl.style.backgroundColor = colorNameEl.textContent;
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
