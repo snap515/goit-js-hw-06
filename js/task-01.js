ec15deccf2538a3c0584e41846b1247d2c191598
Anton Pedan <77937504+snap515@users.noreply.github.com>
2023-01-10 11:59:46 +0100

    update 1/10

--- js/task-01.js
+++ js/task-01.js
@@ -1,7 +1,6 @@
 const categoriesEl = document.querySelectorAll(".item");
-console.log("Number of categories: ", categoriesEl.length);
-
-const categoriesDetails = categoriesEl.forEach((category) => {
-  console.log("Category: ", category.firstElementChild.textContent);
-  console.log("Elements: ", category.querySelectorAll("li").length);
+console.log(`Number of categories ${categoriesEl.length}`);
+categoriesEl.forEach((category) => {
+  console.log(`Category: ${category.firstElementChild.textContent}`);
+  console.log(`Elements: ${category.lastElementChild.childElementCount}`);
 });
